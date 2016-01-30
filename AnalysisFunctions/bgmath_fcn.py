e4762bd608ce7f41a5c2f936527a105a8fbb9de3
Santosh Suram <karthiksuram@gmail.com>
2016-01-29 16:04:17 -0800

    Bgcode math, fom name, linepiecewise fmt changes
    Change in linear piecewise functions, mergelinsegs and name changes for
    fom to match develop_analysis_class

--- AnalysisFunctions/bgmath_fcn.py
+++ AnalysisFunctions/bgmath_fcn.py
@@ -25,11 +25,14 @@ def residuals(params,xdata,ydata): return sum((np.abs(ydata-linpiecewise(params,
    
 # params: zeroth index corresponds to the yfit value at x=x0. Next num_knots indices correspond to the knot positions. Next num_knots-1 indices correspond
 #to slopes for the linear regions.  Piecewise linear addition to obtain the value of fitdata at a specific value of x. 
-def linpiecewise(y0,knots,slopes,x):
-    fitdata=y0
-    for loc in np.arange(0,np.size(knots)-1):
-        fitdata+=slopes[loc]*(x>=(knots[loc]-0.0001))*(np.min(knots[loc+1],x)-(knots[loc]))
-    return fitdata
+def linpiecewise(params,x):
+    fitdata=np.ones(np.shape(x))*params[0]
+    num_knots=len(params)/2
+    for loc in np.arange(1,num_knots):
+        fitdata+=params[num_knots+loc]*(x>=params[loc])*(np.min(np.array(\
+        [np.ones(np.shape(x))*params[loc+1],x]),axis=0)-params[loc])
+    return fitdata    
+    
 
 #xorder: if loc in data represents smaller value of energy to higher value of energy then xorder is increasing otherwise decreasing
 #first constraint in cons is to ensure minimum knot distance min_knotdist. Second equality constraint indicates that the first and the last knot are fixed to the first and last values of x
@@ -41,18 +44,14 @@ def linearfit(xdata,ydata,num_knots,min_knotdist,xorder,options,tol):
     locs=np.arange(1,num_knots+1)
     cons=({'type':'ineq','fun':lambda params: params[locs[0:-1]+1]*sign-(params[locs[0:-1]]*sign+min_knotdist)},\
     {'type':'eq','fun':lambda params: np.array([params[1],params[num_knots]])-np.array([xdata[0],xdata[-1]],)})
-    init_params=np.hstack((ydata[0],np.arange(xdata[0],xdata[-1]+10.**(-4),(xdata[-1]-xdata[0])/(num_knots-1))\
+    init_params=np.hstack((ydata[0],np.arange(xdata[0],xdata[-1]-10.**(-4),(xdata[-1]-xdata[0])/(num_knots-1.))\
     ,np.ones(num_knots-1)*(ydata[-1]-ydata[0])/(xdata[-1]-xdata[0])))
     res = sp.optimize.minimize(residuals,init_params, args=(xdata,ydata),constraints=cons, method='SLSQP',options=options,tol=tol)
     return res
     
 def mergelinsegs(params,num_knots,max_merge_differentialTP,merge_linsegslopediff_percent):
     knots=params[1:num_knots+1]
-    partial_slopes=params[num_knots+1:2*num_knots]
-    slopes=np.empty(np.shape(partial_slopes))
-    for loc in np.arange(0,np.shape(partial_slopes)[0]):
-        slopes[loc]=np.sum(partial_slopes[0:loc+1])
-    np.delete(partial_slopes,np.s_[0::],axis=None)    
+    slopes=params[num_knots+1:2*num_knots]
 
     if merge_linsegslopediff_percent>=1:
         merge_linsegslopediff_percent=merge_linsegslopediff_percent/100.0
@@ -91,11 +90,8 @@ def mergelinsegs(params,num_knots,max_merge_differentialTP,merge_linsegslopediff
             break
     num_knots=j+2
 
-    partial_newslopes=np.empty(np.shape(newslopes)[0])
-    partial_newslopes[0]=newslopes[0]
-    for loc in np.arange(1,np.shape(newslopes)[0]):
-        partial_newslopes[loc]=newslopes[loc]-newslopes[loc-1]   
-    params=np.concatenate(([params[0]],newknots,partial_newslopes),axis=0)
+  
+    params=np.concatenate(([params[0]],knots,slopes),axis=0)
     return params
     
 #The block below creates data for each bg type and interacts with fitting, residual measurement functions to identify the best linear piecewise parameters which are then sent to calc_bandgap for bandgap calculations.
@@ -121,13 +117,10 @@ merge_linsegslopediff_percent,maxtol,min_knotdist=0.05,xorder='increasing',dispr
         tempparams=res.x             
         tempparams[0]-=yoffset
         tempparams=mergelinsegs(tempparams,num_knots,max_merge_differentialTP,merge_linsegslopediff_percent)
-        linfitd,fomd=calc_bandgap(tempparams,np.size(tempparams)/2,min_allowedslope,\
+        linfitd,fomd=calc_bandgap(tempparams,np.size(tempparams)/2,max_numbgs,min_allowedslope,\
         min_bgTP_diff,min_bkgrdslope,min_bgbkgrdslopediff,min_finseglength,merge_bgslopediff_percent,\
         min_TP_finseg_diff,min_bgfinalseglength)
-        data['linfit']=linpiecewise(linfitd['y0'],linfitd['knots'],linfitd['slopes'],data['E'])
-        fomd['bg_repr']=fomd['bg_'+str(np.argmax([fomd['abs_expl_'+str(idx)] for idx \
-        in xrange(max_numbgs) if not np.isnan(fomd['abs_expl_'+str(idx)])]))]
-        fomd['bgcode0-only']=fomd['bg_0'] if fomd['bgcode_1']==np.nan and fomd['bgcode_0']==0 else np.NaN
+        data['linfit']=linpiecewise(np.concatenate(([linfitd['y0']],linfitd['knots'],linfitd['slopes']),axis=0),data['E'])
     return [linfitd,fomd]
             
 def calc_bandgap(params,num_knots,max_numbgs,min_allowedslope,min_bgTP_diff,min_bkgrdslope,min_bgbkgrdslopediff,\
@@ -146,123 +139,127 @@ min_finseglength,merge_bgslopediff_percent,min_TP_finseg_diff ,min_bgfinalseglen
     for i in np.arange(0,tot_segs-1):
         if slopes[i]<min_allowedslope:
             num_segments=i+1
-#THERE COULD BE SOME PROBLEM HERE REVISIT
-#            bgcode=[1]
             break
-    for i in np.arange(1,num_segments-1):
-#        if 1 not in bgcode:
-#==============================================================================
-#           TPdiff.extend([(knots[i+1]-knots[i])*slopes[i]])min_TP_finseg_diff 
-#==============================================================================
-            if slopes[i]>slopes[i-1] and slopes[i]>0 and slopes[i-1]>min_bkgrdslope:
-                TPdiff=(knots[i+1]-knots[i])*slopes[i]
-                if TPdiff>=min_bgTP_diff:
-                    if slopes[i]>slopes[i+1]:    
-                        bgknots_lower.extend([i])
-                        abs_expl.extend([TPdiff])
-                        bgcode.extend([0])
-                    else:            
-                        if slopes[i+1]-slopes[i]<merge_bgslopediff_percent*(slopes[i]):
-                            if i==num_segments-2 or (i!=num_segments-2 and slopes[i+2]<slopes[i+1]):
-                                TPdiff=(knots[i+1]-knots[i])*slopes[i]
-                                if TPdiff>=min_bgTP_diff:
-                                    bgknots_lower.extend([i])
-                                    abs_expl.extend([TPdiff])
-                                    bgcode.extend([2])
-    if np.size(bgknots_lower)==0:
-        j=num_segments-1
-        if slopes[j]>slopes[j-1] and slopes[j]>0 and slopes[j-1]>min_bkgrdslope:
-          TPdiff=(knots[j+1]-knots[j])*slopes[j]
-          if TPdiff>=min_TP_finseg_diff and knots[j+1]-knots[j]>min_bgfinalseglength:
-              bgknots_lower.extend([j])
-              abs_expl.extend([TPdiff])
-              bgcode.extend([6])
-   
-    if np.size(bgknots_lower)!=0:
-        for i in np.arange(0,np.shape(bgknots_lower)[0]):
-            if i==0: low_limit=-1
-            else: low_limit=bgknots_lower[i-1]
-            if bgknots_lower[i]-1==low_limit:
-                del bgknots_lower[i::]  
-                del abs_expl[i::]
-                del bgcode[i::]
-                break
-#            A band gap segments bkgrnd segment cannot be previous band gap segment
-            for loc in np.arange(bgknots_lower[i]-1,low_limit,-1):
-                if slopes[loc]<slopes[loc+1] and slopes[loc]>min_bkgrdslope:
-                    if loc==low_limit+1:
-                        bkgrdknots_lower.extend([loc])
-#                        This block gets executed when the bkgrd segment is just to the right of the previous band gap segment
-                        break
-                    else: continue
-                elif loc!=bgknots_lower[i]-1:
-                    bkgrdknots_lower.extend([loc+1])                    
-                    break
-                else: 
-                    temp=np.shape(bgknots_lower)[0]
+    if num_segments==tot_segs:
+    
+        for i in np.arange(1,num_segments-1):
+    #        if 1 not in bgcode:
+    #==============================================================================
+    #           TPdiff.extend([(knots[i+1]-knots[i])*slopes[i]])min_TP_finseg_diff 
+    #==============================================================================
+                if slopes[i]>slopes[i-1] and slopes[i]>0 and slopes[i-1]>min_bkgrdslope:
+                    TPdiff=(knots[i+1]-knots[i])*slopes[i]
+                    if TPdiff>=min_bgTP_diff:
+                        if slopes[i]>slopes[i+1]:    
+                            bgknots_lower.extend([i])
+                            abs_expl.extend([TPdiff])
+                            bgcode.extend([0])
+                        else:            
+                            if slopes[i+1]-slopes[i]<merge_bgslopediff_percent*(slopes[i]):
+                                if i==num_segments-2 or (i!=num_segments-2 and slopes[i+2]<slopes[i+1]):
+                                    TPdiff=(knots[i+1]-knots[i])*slopes[i]
+                                    if TPdiff>=min_bgTP_diff:
+                                        bgknots_lower.extend([i])
+                                        abs_expl.extend([TPdiff])
+                                        bgcode.extend([2])
+        if np.size(bgknots_lower)==0:
+            j=num_segments-1
+            if slopes[j]>slopes[j-1] and slopes[j]>0 and slopes[j-1]>min_bkgrdslope:
+              TPdiff=(knots[j+1]-knots[j])*slopes[j]
+              if TPdiff>=min_TP_finseg_diff and knots[j+1]-knots[j]>min_bgfinalseglength:
+                  bgknots_lower.extend([j])
+                  abs_expl.extend([TPdiff])
+                  bgcode.extend([6])
+       
+        if np.size(bgknots_lower)!=0:
+            for i in np.arange(0,np.shape(bgknots_lower)[0]):
+                if i==0: low_limit=-1
+                else: low_limit=bgknots_lower[i-1]
+                if bgknots_lower[i]-1==low_limit:
                     del bgknots_lower[i::]  
                     del abs_expl[i::]
                     del bgcode[i::]
-                    i=temp
-#                    This block should never get executed because any bgknot will have corresponding bkgrdknot except
-#                    when the previous segment is also a band gap segment taken care of bgknots_lower[i]-1==low_limit block above
-
-    if np.size(bgknots_lower)!=0:
-        if not (np.size(bgcode)==1 and bgcode[0]==1):
-
-            if not (((np.size(abs_expl)==np.size(bgknots_lower)) and (np.size(bgknots_lower)==np.size(bkgrdknots_lower))) and (np.size(np.array(bgcode)[np.where(np.not_equal(bgcode,1))[0]])==np.size(bkgrdknots_lower))):
-                raise ValueError('abs_expl,bgknots_lower,bgcodes and bkgrdknots_lower do not have the same size')
-        for i in np.arange(0,np.shape(bkgrdknots_lower)[0]):
-            if (slopes[bgknots_lower[i]]-slopes[bkgrdknots_lower[i]])<min_bgbkgrdslopediff:
-                bgknots_lower[i],bkgrdknots_lower[i],abs_expl[i]=-1000*np.ones([3,])
-                bgcode.extend([4])
-        bgknots_lower=filter(lambda a: a != -1000, bgknots_lower)
-        bkgrdknots_lower=filter(lambda a: a != -1000, bkgrdknots_lower)
-        abs_expl=filter(lambda a: a != -1000, abs_expl)
-
-        if np.size(bgknots_lower)==0:
-            bgknots_lower,bkgrdknots_lower,bg,abs_expl=np.ones([max_numbgs,1])*np.NaN
-            bgcode.extend([5])
-        else:   
-            for i in np.arange(0,np.shape(bgknots_lower)[0]):
-                [y1,y2]=linpiecewise(params,[knots[bgknots_lower][i],knots[bkgrdknots_lower][i]])
-                [m1,m2]=[slopes[bgknots_lower[i]],slopes[bkgrdknots_lower[i]]]
-                [x1,x2]=[knots[bgknots_lower[i]],knots[bkgrdknots_lower[i]]]
-                bg.extend([(y1-y2-(m1*x1-m2*x2))/(m2-m1)])
-                
-    else:
-        bgknots_lower,bkgrdknots_lower,bg,abs_expl=np.ones([4,1])*np.NaN        
-        bgcode.extend([3])
-    if num_segments!=tot_segs:
-        if np.size(bgcode)==0:
-            bgcode=[1]
+                    break
+    #            A band gap segments bkgrnd segment cannot be previous band gap segment
+                for loc in np.arange(bgknots_lower[i]-1,low_limit,-1):
+                    if slopes[loc]<slopes[loc+1] and slopes[loc]>min_bkgrdslope:
+                        if loc==low_limit+1:
+                            bkgrdknots_lower.extend([loc])
+    #                        This block gets executed when the bkgrd segment is just to the right of the previous band gap segment
+                            break
+                        else: continue
+                    elif loc!=bgknots_lower[i]-1:
+                        bkgrdknots_lower.extend([loc+1])                    
+                        break
+                    else: 
+                        temp=np.shape(bgknots_lower)[0]
+                        del bgknots_lower[i::]  
+                        del abs_expl[i::]
+                        del bgcode[i::]
+                        i=temp
+    #                    This block should never get executed because any bgknot will have corresponding bkgrdknot except
+    #                    when the previous segment is also a band gap segment taken care of bgknots_lower[i]-1==low_limit block above
+    
+        if np.size(bgknots_lower)!=0:
+            if not (np.size(bgcode)==1 and bgcode[0]==1):
+    
+                if not (((np.size(abs_expl)==np.size(bgknots_lower)) and (np.size(bgknots_lower)==np.size(bkgrdknots_lower))) and (np.size(np.array(bgcode)[np.where(np.not_equal(bgcode,1))[0]])==np.size(bkgrdknots_lower))):
+                    raise ValueError('abs_expl,bgknots_lower,bgcodes and bkgrdknots_lower do not have the same size')
+            for i in np.arange(0,np.shape(bkgrdknots_lower)[0]):
+                if (slopes[bgknots_lower[i]]-slopes[bkgrdknots_lower[i]])<min_bgbkgrdslopediff:
+                    bgknots_lower[i],bkgrdknots_lower[i],abs_expl[i]=-1000*np.ones([3,])
+                    bgcode.extend([4])
+            bgknots_lower=filter(lambda a: a != -1000, bgknots_lower)
+            bkgrdknots_lower=filter(lambda a: a != -1000, bkgrdknots_lower)
+            abs_expl=filter(lambda a: a != -1000, abs_expl)
+    
+            if np.size(bgknots_lower)==0:
+                bgknots_lower,bkgrdknots_lower,bg,abs_expl=np.ones([4,1])*np.NaN
+                bgcode.extend([5])
+            else:   
+                for i in np.arange(0,np.shape(bgknots_lower)[0]):
+                    [y1,y2]=linpiecewise(params,[knots[bgknots_lower][i],knots[bkgrdknots_lower][i]])
+                    [m1,m2]=[slopes[bgknots_lower[i]],slopes[bkgrdknots_lower[i]]]
+                    [x1,x2]=[knots[bgknots_lower[i]],knots[bkgrdknots_lower[i]]]
+                    bg.extend([(y1-y2-(m1*x1-m2*x2))/(m2-m1)])
+                    
         else:
-            for idx,val in enumerate(bgcode):
-                bgcode[idx]=[1.+val/100.]
-                
+            bgknots_lower,bkgrdknots_lower,bg,abs_expl=np.ones([4,1])*np.NaN        
+            bgcode.extend([3])
+    elif num_segments!=tot_segs:
+        if np.size(bgcode)==0:
+            bgknots_lower,bkgrdknots_lower,bg,abs_expl=np.ones([4,1])*np.NaN        
+            bgcode.extend([1])
+
     fomd=dict([(lstk+'_'+str(idx),eval(lstk)[idx]) for lstk in ['bgknots_lower', \
-    'bkgrdknots_lower', 'bg','abs_expl','bgcode'] for idx in xrange(max_numbgs)])
+    'bkgrdknots_lower', 'bg','abs_expl','bgcode'] for idx in xrange(min(len(bg),max_numbgs))])
+
     linfitd=dict(dict([(lstk+'_'+str(idx),eval(lstk)[idx]) for lstk in ['bgknots_lower', \
-    'bkgrdknots_lower'] for idx in xrange(max_numbgs)]),knots=knots,slopes=slopes,y0=params[0])
+    'bkgrdknots_lower'] for idx in xrange(min(len(bg),max_numbgs))]),knots=knots,slopes=slopes,y0=params[0])
+    if not np.isnan(abs_expl).all():
+        x=np.argmax([fomd['abs_expl_'+str(idx)] for idx\
+        in xrange(min(len(bg),max_numbgs)) if not np.isnan(fomd['abs_expl_'+str(idx)])])
+        fomd['bg_repr']=fomd['bg_'+str(x)]
+        fomd['bgcode_repr']=fomd['bgcode_'+str(x)]
+    else:
+        fomd['bg_repr']=np.nan
+        fomd['bgcode_repr']=np.nan
+    
+    fomd['bgcode0_only']=fomd['bg_0'] if len(bg)==1 and fomd['bgcode_0']==0 else np.NaN
     return [linfitd,fomd]
     
 
     
-def identifypeaks(data,typ,delta_1stderiv,max_allowed_2ndderiv):\
+def identifypeaks(data,typ,abs_minallowedslope,max_allowed_2ndderiv):\
 return len(np.where(np.logical_and((np.abs(data[typ+'_2ndderiv'])>=max_allowed_2ndderiv),\
-(np.abs(data[typ+'_1stderiv'])<=delta_1stderiv)))[0])>0
+(np.abs(data[typ+'_1stderiv'])<=abs_minallowedslope)))[0])>0
 
 
-def runuvvis(data,inputvars,fomnames):
+def runuvvis(data,inputvars):
 #    Energy should be part of the initial intermediate data since this is needed for visualization
     data['E']=1239.8/data['wl']
 #####
-    bgtyp=inputvars['analtypes'][0]
-    if np.isnan(data[bgtyp]).any():
-        return [{'DA-bgcode_0':8},{}]
-#Implementation of second round of filtering in cases where peaks exist is currently unsupported
-    if identifypeaks(data,'abs',inputvars['delta_1stderiv'],inputvars['max_absolute_2ndderiv']):
-        return [{'DA-bgcode_0':7},{}]
+
     
     code0='Successful assignment of bandgap linear segment using simple rules'
     code1='Linear segment with a slope less than min_slope was found'
@@ -275,18 +272,26 @@ def runuvvis(data,inputvars,fomnames):
     code7='Peaks were found in the mthd spectrum'
     code8='NaNs were found in the absorption spectrum'
     code9='Linear fitting failed'
-    
-    linfitd,fomd=fitresult(data,bgtyp,\
-    num_knots=inputvars['num_knots'],tol=inputvars['tol'],min_allowedslope=inputvars['min_allowedslope'],\
-    min_bgTP_diff=inputvars['min_bgTP_diff'],min_bkgrdslope=inputvars['min_bkgrdslope'],\
-    min_bgbkgrdslopediff=inputvars['min_bgbkgrdslopediff'],min_finseglength=inputvars['min_finseglength'],\
-    merge_bgslopediff_percent=inputvars['merge_bgslopediff_percent'],min_TP_finseg_diff=inputvars['min_TP_finseg_diff'],\
-    min_bgfinalseglength=inputvars['min_bgfinalseglength'],max_merge_differentialTP=inputvars['max_merge_differentialTP'],\
-    merge_linsegslopediff_percent=inputvars['merge_linsegslopediff_percent'],maxtol=inputvars['maxtol'],\
-    min_knotdist=inputvars['min_knotdist'],xorder='increasing',dispresult=False)
+    fomd={};linfitd={}
+    for bgtyp in inputvars['analysis_types']:
+        if np.isnan(data[bgtyp]).any():
+            fomd['bgcode_0']=8
+            continue
+#Implementation of second round of filtering in cases where peaks exist is currently unsupported
+        if identifypeaks(data,'abs',inputvars['abs_minallowedslope'],inputvars['max_absolute_2ndderiv']):
+            fomd['bgcode_0']=7
+            continue
+        linfitd,fomd=fitresult(data,bgtyp,max_numbgs=inputvars['maxbgspersmp'],\
+        num_knots=inputvars['num_knots'],tol=inputvars['tol'],min_allowedslope=inputvars['min_allowedslope'],\
+        min_bgTP_diff=inputvars['min_bgTP_diff'],min_bkgrdslope=inputvars['min_bkgrdslope'],\
+        min_bgbkgrdslopediff=inputvars['min_bgbkgrdslopediff'],min_finseglength=inputvars['min_finseglength'],\
+        merge_bgslopediff_percent=inputvars['merge_bgslopediff_percent'],min_TP_finseg_diff=inputvars['min_TP_finseg_diff'],\
+        min_bgfinalseglength=inputvars['min_bgfinalseglength'],max_merge_differentialTP=inputvars['max_merge_differentialTP'],\
+        merge_linsegslopediff_percent=inputvars['merge_linsegslopediff_percent'],maxtol=inputvars['maxtol'],\
+        min_knotdist=inputvars['min_knotdist'],xorder='increasing',dispresult=False)
 
-    for dct in ['linfitd','fomd']:    
-        for key in eval(dct).keys():
-            if bgtyp not in key:
-                dct[bgtyp+'-'+key]=eval(dct).pop(key)
+        for dct in [linfitd,fomd]:    
+            for key in dct.keys():
+                if bgtyp not in key:
+                    dct[bgtyp+'_'+key]=dct.pop(key)
     return linfitd,fomd
