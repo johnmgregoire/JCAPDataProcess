03467340650761bcd532feb8b2b1fe11754792e1
Dan Guevarra <guevarra@htejcap.caltech.edu>
2016-11-04 11:07:59 -0700

    Fixed anadict['description'] import

--- ExternalDataImportApp.py
+++ ExternalDataImportApp.py
@@ -35,7 +35,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
     def __init__(self, parent=None, title='', folderpath=None):
         super(extimportDialog, self).__init__(parent)
         self.setupUi(self)
-        
+
         self.parent=parent
 
 
@@ -49,7 +49,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         (self.AddMiscAnaPushButton, self.add_misc_to_ana), \
         (self.RaiseErrorPushButton, self.raiseerror), \
         ]
-       
+
 
 
 
@@ -60,48 +60,48 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
 
         for button, fcn in button_fcn:
             QObject.connect(button, SIGNAL("pressed()"), fcn)
-        
+
         QObject.connect(self.CalcXLineEdit,SIGNAL("editingFinished()"),self.lookupsamples)
         QObject.connect(self.CalcYLineEdit,SIGNAL("editingFinished()"),self.lookupsamples)
 
-    
+
         #QObject.connect(self.RunSelectTreeWidget, SIGNAL('itemChanged(QTreeWidgetItem*, int)'), self.runselectionchanged)
         QObject.connect(self.RcpTreeWidget, SIGNAL('itemDoubleClicked(QTreeWidgetItem*, int)'), self.edittreeitem_readonly)
         QObject.connect(self.ExpTreeWidget, SIGNAL('itemDoubleClicked(QTreeWidgetItem*, int)'), self.edittreeitem_exp)
         QObject.connect(self.AnaTreeWidget, SIGNAL('itemDoubleClicked(QTreeWidgetItem*, int)'), self.edittreeitem_ana)
         QObject.connect(self.RunSelectTreeWidget, SIGNAL('itemDoubleClicked(QTreeWidgetItem*, int)'), self.edittreeitem)
         self.runtreeclass=treeclass_filedict(self.RunSelectTreeWidget)
-        
-        
-        
+
+
+
         self.profiledefintionfcns=[self.xrds_profile]
-                                      
+
         profiledesc=['Bruker XRDS data']
         for i, l in enumerate(profiledesc):
             self.ProfileComboBox.insertItem(i, l)
-            
 
-        
+
+
         self.RcpTreeWidgetFcns=treeclass_filedict(self.RcpTreeWidget)
         self.ExpTreeWidgetFcns=treeclass_filedict(self.ExpTreeWidget)
         self.AnaTreeWidgetFcns=treeclass_filedict(self.AnaTreeWidget)
 
         self.cleardata()
 
-    
+
     def xrds_profile(self):
         self.importfolderfcn=get_rcpdlist_xrdolfder
         self.createfiledictsfcn=self.createfiledicts_xrds
         self.datatype='xrds'
-        
+
     def raiseerror(self):
         raiseerror
-    
+
     def stdcopy_afd(self, afd, anafolder):
         p=os.path.join(afd['folderpath'], afd['fn'])
         newp=os.path.join(anafolder, afd['anafn'])
         shutil.copy(p, newp)
-        
+
     def xrds_copy_xy_to_ana(self, afd, anafolder):
         p=os.path.join(afd['folderpath'], afd['fn'])
         newp=os.path.join(anafolder, afd['anafn'])
@@ -112,7 +112,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         s='\n'.join(lines)
         with open(newp, mode='w') as f:
             f.write(s)
-            
+
     def createfiledicts_xrds(self):
         self.inds_rcpdlist=self.runtreeclass.getindsofchecktoplevelitems()
         self.ana_filedict_tocopy=[]
@@ -126,7 +126,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         anabool=int(self.AnaSaveComboBox.currentIndex())>0
         for runcount, ind_rcp in enumerate(self.inds_rcpdlist):
             rcpd=self.maindatad['rcpdlist'][ind_rcp]
-            
+
             rcpdict={}
             rcpdict['experiment_type']=self.datatype
             runk='run__%d' %(runcount+1)
@@ -136,11 +136,11 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             exprund['plate_id']=self.plate_idstr
             rcpd['parameters']['plate_id']=self.plate_idstr
             exprund['rcp_file']=rcpd['name']+'.rcp'
-            
+
             for k in ['name', 'parameters']:
                 rcpdict[k]=rcpd[k]
                 exprund[k]=rcpd[k]
-            
+
             for fd in rcpd['file_dlist']:
                 if not fd['tech'] in rcpdict.keys():
                     rcpdict[fd['tech']]={}
@@ -183,8 +183,8 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
                                 if not anrunk in ana__d.keys():
                                     ana__d[anrunk]={}
                                 ana__d=ana__d[anrunk]
-                                
-                                
+
+
                             newfn='%s_%s' %(anak, afd['fn'].replace('.xy', '.csv'))
                             afd['anafn']=newfn
                             afd['copy_fcn']=self.xrds_copy_xy_to_ana
@@ -196,12 +196,12 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             self.all_rcp_dict['rcp_file__%d' %(runcount+1)]=rcpdict
         if anabool:
             self.anadict['plate_ids']=self.plate_idstr
-            self.anadict['description']='%s on plate_id %s' %(','.join(set([anad['name'] for anak, anad in self.anadict.itervalues() if anak.startswith('ana__')])), self.plate_idstr)
+            self.anadict['description']='%s on plate_id %s' %(','.join(set([anad['name'] for anak, anad in self.anadict.iteritems() if anak.startswith('ana__')])), self.plate_idstr)
             self.anadict['ana_version']='3'
         self.RcpTreeWidgetFcns.filltree(self.all_rcp_dict, startkey='', laststartswith='rcp_file__', updatedwithtoplevelitems=True)
         self.ExpTreeWidgetFcns.filltree(self.expdict, startkey='exp_version', laststartswith='run__')
         self.AnaTreeWidgetFcns.filltree(self.anadict, startkey='ana_version', laststartswith='ana__')
-    
+
     def add_misc_to_ana(self, p=None, anak=None, anarunk=None):
         if anak is None or anarunk is None:
             if not 'ana__1' in self.anadict.keys():
@@ -209,13 +209,13 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             l_anarunk=sort_dict_keys_by_counter(self.anadict['ana__1'], keystartswith='files_run__')
             if len(l_anarunk)==0:
                 return
-            
+
             ans=userinputcaller(self, inputs=[('ana__ key', str, 'ana__1'), ('files_run__ key key', str, l_anarunk[0])], title='Enter location in .ana',  cancelallowed=True)
             if readonly or ans is None or ans[0].strip()==v:
                 return
             anak=ans[0].strip()
             anarunk=ans[1].strip()
-            
+
         if not anak in self.anadict.keys():
             return
         if not anarunk in self.anadict[anak].keys():
@@ -260,7 +260,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         if readonly or ans is None or ans[0].strip()==v:
             return
         ans=ans[0].strip()
-        
+
         warningbool = (True in [ws in k for ws in ['version']])
         parent=item.parent()
         if parent is None:
@@ -268,17 +268,17 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         else:
             parentstr=parent.text(0)
         warningbool = warningbool or True in [ws in parentstr or ws in k for ws in ['__', 'parameters', 'files_']]
-        
-        
+
+
         if warningbool:
             idialog=messageDialog(self, 'THIS IS CONSIDERED A READ-ONLY PARAMETER.\nYOU SHOULD PROBABLY "Cancel"')
             if not idialog.exec_():
                 return
         item.setText(column,''.join([k, ans]))
-        
+
         if d is None:
             return
-            
+
         kl=[k.partition(':')[0].strip()]
         while not item.parent() is None:
             item=item.parent()
@@ -300,8 +300,8 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
                 return
             pid=ans[0]
         self.plate_idstr=pid
-        
-        
+
+
         self.cleardata()
         self.profiledefintionfcns[self.ProfileComboBox.currentIndex()]()#defines the functions
         self.maindatad=self.importfolderfcn(p)
@@ -309,8 +309,8 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         if self.maindatad is None:
             return
         self.plateidLineEdit.setText(self.plate_idstr)
-        
-        
+
+
         for count, rcpd in enumerate(self.maindatad['rcpdlist']):
             mainitem=QTreeWidgetItem(['temprun__%d' %(count+1)], 0)
 
@@ -326,9 +326,9 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
                         fd['treeitem'].addChild(item)
             self.RunSelectTreeWidget.addTopLevelItem(mainitem)
             mainitem.setExpanded(True)
-            
+
         self.runtreeclass.maketoplevelchecked()
- 
+
 
         self.foldernameLineEdit.setText(p)
         self.openplatemap(self.plate_idstr)
@@ -362,11 +362,11 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         if self.createfiledictsfcn is None:
             return
         self.createfiledictsfcn()
-        
+
 
     def xrds_create_rcp_exp_ana(self):
         self.updateana()
-        
+
     def cleardata(self, anadict=None):
         self.inds_rcpdlist=[]
         self.ana_filedict_tocopy=[]
@@ -374,7 +374,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         self.maindatad={}
         self.expdict={}
         self.anadict={}
-        
+
         self.importfolderfcn=None
         self.createfiledictsfcn=None
         self.plotw_plate.axes.cla()
@@ -387,15 +387,15 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         self.ExpTreeWidget.clear()
         self.AnaTreeWidget.clear()
 
- 
- 
+
+
     def savefiles(self):
-        
+
         dropfolder=tryprependpath(EXPERIMENT_DROP_FOLDERS, os.path.join(self.datatype, 'drop'))
         if dropfolder is None:
             messageDialog(self, 'Aborting SAVE because cannot find drop folder').exec_()
             return
-        
+
         for runcount, ind_rcp in enumerate(self.inds_rcpdlist):
             rcpfiled=self.all_rcp_dict['rcp_file__%d' %(runcount+1)]
             rcpname=rcpfiled['name']
@@ -403,22 +403,22 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             if os.path.isdir(rcpfolder):
                 messageDialog(self, 'Aborting SAVE because at least 1 rcp drop folder exists').exec_()
                 return
-        
+
         for runcount, ind_rcp in enumerate(self.inds_rcpdlist):
             rcpfiled=self.all_rcp_dict['rcp_file__%d' %(runcount+1)]
             rcpname=rcpfiled['name']
             rcpfolder=os.path.join(dropfolder, rcpname)
             os.mkdir(rcpfolder)
-            
+
             rcpd=self.maindatad['rcpdlist'][ind_rcp]
             for fd in rcpd['file_dlist']:
                 shutil.copy(os.path.join(fd['folderpath'], fd['fn']), os.path.join(rcpfolder, fd['fn']))
-            
+
             rcppath=os.path.join(rcpfolder, rcpname+'.rcp')
             filestr=self.RcpTreeWidgetFcns.createtxt(parentitem=rcpfiled['treeitem'])
             with open(rcppath, mode='w') as f:
                 f.write(filestr)
-            
+
         saveopt=int(self.ExpSaveComboBox.currentIndex())
         if self.expdict is None or len(self.expdict)==0:
             saveopt=0
@@ -429,7 +429,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             expname=expfn.rpartition('.')[0]
         else:
             return #cannot save ana if not saving exp
-            
+
         saveopt=int(self.AnaSaveComboBox.currentIndex())
         if self.anadict is None or len(self.anadict)==0:
             saveopt=0
@@ -438,13 +438,13 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             self.anadict['experiment_path']=rp.replace(chr(92),chr(47))
             self.anadict['experiment_name']=expname
             self.AnaTreeWidgetFcns.filltree(self.anadict, startkey='ana_version', laststartswith='ana__')
-            
+
             anatempfolder=getanadefaultfolder()
             anafilestr=self.AnaTreeWidgetFcns.createtxt()
             for afd in self.ana_filedict_tocopy:
                 afd['copy_fcn'](afd, anatempfolder)# could be shutil.copy if no changes required
             anafolder=saveana_tempfolder(anafilestr, anatempfolder, anadict=None, analysis_type='temp' if saveopt==1 else self.datatype, rundone='.done' if saveopt==3 else '.run')
-        
+
 #        if saveopt>1:#everything now in the folder so copy to non-temp if necessary
 #            saveana_tempfolder(None, anafolder, anadict=None, skipana=False, analysis_type=self.datatype, rundone='.done' if saveopt==3 else '.run')
 
@@ -461,17 +461,17 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
                 return
         else:
             fcnx, fcny=xyfcns
-        
+
         xall, yall=numpy.array([fd['xyarr'] for count, rcpd in enumerate(self.maindatad['rcpdlist']) for fd in rcpd['file_dlist'] if 'xyarr' in fd.keys()]).T
-        
+
         xpmall=fcnx(xall, yall)
         ypmall=fcny(xall, yall)
         xypmall=list(numpy.array([xpmall, ypmall]).T)
-        
+
         pmx=numpy.array([d['x'] for d in self.platemapdlist])
         pmy=numpy.array([d['y'] for d in self.platemapdlist])
         pms=[d['sample_no'] for d in self.platemapdlist]
-        
+
 
         xyplotinfo=[]
         for count, rcpd in enumerate(self.maindatad['rcpdlist']):
@@ -488,7 +488,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         self.plot(xyplotinfo)
 
     def plot(self, xyplotinfo):
-        
+
         self.plotw_plate.axes.cla()
         ax=self.plotw_plate.axes
         xl=[]
@@ -504,7 +504,7 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
             return
         circ = pylab.Circle((50, 47.3), radius=50., edgecolor='k', facecolor='none')
         ax.add_patch(circ)
-        
+
         x0, x1=0, 100
         y0, y1=0, 97.3
         x0=min(x0, min(xl))
@@ -513,28 +513,28 @@ class extimportDialog(QDialog, Ui_ExternalImportDialog):
         y1=max(y1, max(yl))
         ax.set_xlim(x0, x1)
         ax.set_ylim(y0, y1)
-        
+
         self.plotw_plate.fig.canvas.draw()
-        
-     
+
+
 
     def plotwsetup(self):
 
         self.plotw_plate=plotwidget(self)
-        
+
 
         for b, w in [\
             (self.textBrowser_plate, self.plotw_plate), \
             ]:
             w.setGeometry(b.geometry())
             b.hide()
-        
+
         self.plotw_plate.axes.set_aspect(1)
 
         axrect=[0.88, 0.1, 0.04, 0.8]
 
         self.plotw_plate.fig.subplots_adjust(left=0.12, bottom=.12)
-    
+
 
 
 
@@ -542,7 +542,7 @@ class treeclass_filedict():
     def __init__(self, tree):
         self.treeWidget=tree
         self.treeWidget.clear()
-    
+
     def getlistofchecktoplevelitems(self):
         return [str(self.treeWidget.topLevelItem(count).text(0)).strip().strip(':') for count in range(int(self.treeWidget.topLevelItemCount()))\
             if bool(self.treeWidget.topLevelItem(count).checkState(0))]
@@ -557,16 +557,16 @@ class treeclass_filedict():
     def filltree(self, d, startkey='ana_version', laststartswith='ana__', updatedwithtoplevelitems=False):
         self.treeWidget.clear()
         #assume startkey is not for dict and laststatswith is dict
-        
+
         if len(startkey)>0:
             mainitem=QTreeWidgetItem([': '.join([startkey, d[startkey]])], 0)
             self.treeWidget.addTopLevelItem(mainitem)
             self.treeWidget.setCurrentItem(mainitem)
-        
+
         for k in sorted([k for k, v in d.iteritems() if k!=startkey and not isinstance(v, dict) and not k=='treeitem']):
             mainitem=QTreeWidgetItem([': '.join([k, str(d[k])])], 0)
             self.treeWidget.addTopLevelItem(mainitem)
-            
+
         for k in sorted([k for k, v in d.iteritems() if not k.startswith(laststartswith) and isinstance(v, dict)]):
             mainitem=QTreeWidgetItem([k+':'], 0)
             self.nestedfill(d[k], mainitem)
@@ -574,7 +574,7 @@ class treeclass_filedict():
             mainitem.setExpanded(False)
             if updatedwithtoplevelitems:
                 d[k]['treeitem']=mainitem
-            
+
         anakl=sort_dict_keys_by_counter(d, keystartswith=laststartswith)
         for k in anakl:
             mainitem=QTreeWidgetItem([k+':'], 0)
@@ -583,8 +583,8 @@ class treeclass_filedict():
             mainitem.setExpanded(False)
             if updatedwithtoplevelitems and isinstance(d[k], dict):
                 d[k]['treeitem']=mainitem
-            
-        
+
+
     def fillmainitem_with_dlistvalues(self, mainitem, dlist, k='k', v='v', updatedwithitems=True, checkbool=None):
         for d in dlist:
             item=QTreeWidgetItem([': '.join([str(d[k]), str(d[v])])], 0)
@@ -616,7 +616,7 @@ class treeclass_filedict():
         else:
             itemlist=[parentitem.child(count) for count in range(int(parentitem.childCount()))]
         return '\n'.join([self.createtxt_item(item) for item in itemlist])
-        
+
     def createtxt_item(self, item, indentlevel=0):
         str(item.text(0))
         itemstr=self.indent*indentlevel+str(item.text(0)).strip()
@@ -624,7 +624,7 @@ class treeclass_filedict():
             return itemstr
         childstr='\n'.join([self.createtxt_item(item.child(i), indentlevel=indentlevel+1) for i in range(item.childCount())])
         return '\n'.join([itemstr, childstr])
-    
+
     def partitionlineitem(self, item):
         s=str(item.text(0)).strip()
         a, b, c=s.partition(':')
@@ -633,7 +633,7 @@ class treeclass_filedict():
         return dict(\
         [self.createdict_item(self.treeWidget.topLevelItem(count))\
             for count in range(int(self.treeWidget.topLevelItemCount()))])
-        
+
     def createdict_item(self, item):
         tup=self.partitionlineitem(item)
         if item.childCount()==0:
@@ -657,4 +657,3 @@ if __name__ == "__main__":
     form.show()
     form.setFocus()
     mainapp.exec_()
-
