4477a20d7cedee47890cc0eb4047a9fc9967d41b
johnmgregoire <john.gregoire@solarfuleshub.org>
2016-11-09 20:18:00 -0800

    change vis back to making placeholder fom for each ana__

--- AuxPrograms/VisualizeAuxFcns.py
+++ AuxPrograms/VisualizeAuxFcns.py
@@ -87,30 +87,32 @@ def readandformat_anafomfiles(anafolder, anafiledict, l_fomdlist, l_fomnames, l_
                     l_platemapkeys+=[pmkeys]
                     treefcns.appendFom(keys, csvheaderdict, anak=anak, anad=anad)
                     foundafomfile=True
-    if not foundafomfile:
-        fomdlist=[]
-        foundsmps=[]
-        for anarunk, anarund in anad.iteritems():
-            if not anarunk.startswith('files_run__'):
-                continue
-            runint=eval(anarunk.partition('files_run__')[2])
-            for typek, typed in anarund.iteritems():
-                newsmps=[filed['sample_no'] for filek, filed in typed.iteritems() if 'sample_no' in filed.keys() and filed['sample_no']>0 and not filed['sample_no'] in foundsmps]
-                fomdlist+=[{'sample_no':smp, 'anaint':anaint, 'runint':runint} for smp in newsmps]
-                foundsmps+=newsmps
-        if len(fomdlist)>0:
-            keys=fomdlist[0].keys()
-            l_fomdlist+=[fomdlist]
-            l_fomnames+=[keys]
-            csvheaderdict={}
-            csvheaderdict['anak']=anak
-            l_csvheaderdict+=[csvheaderdict]
-            if 'platemap_comp4plot_keylist' in anad.keys():
-                pmkeys=anad['platemap_comp4plot_keylist'].split(',')
-            else:
-                pmkeys=platemap4keys_default
-            l_platemapkeys+=[pmkeys]
-            treefcns.appendFom(keys, csvheaderdict, anak=anak, anad=anad)
+        if not foundafomfile:
+            fomdlist=[]
+            foundsmps=[]
+           
+            pid=anad['plate_ids'] if isinstance(anad['plate_ids'], int) else 0
+            for anarunk, anarund in anad.iteritems():
+                if not anarunk.startswith('files_run__'):
+                    continue
+                runint=eval(anarunk.partition('files_run__')[2])
+                for typek, typed in anarund.iteritems():
+                    newsmps=[filed['sample_no'] for filek, filed in typed.iteritems() if 'sample_no' in filed.keys() and filed['sample_no']>0 and not filed['sample_no'] in foundsmps]
+                    fomdlist+=[{'sample_no':smp, 'anaint':anaint, 'runint':runint, 'plate_id':pid} for smp in newsmps]
+                    foundsmps+=newsmps
+            if len(fomdlist)>0:
+                keys=fomdlist[0].keys()
+                l_fomdlist+=[fomdlist]
+                l_fomnames+=[keys]
+                csvheaderdict={}
+                csvheaderdict['anak']=anak
+                l_csvheaderdict+=[csvheaderdict]
+                if 'platemap_comp4plot_keylist' in anad.keys():
+                    pmkeys=anad['platemap_comp4plot_keylist'].split(',')
+                else:
+                    pmkeys=platemap4keys_default
+                l_platemapkeys+=[pmkeys]
+                treefcns.appendFom(keys, csvheaderdict, anak=anak, anad=anad)
                     
 class legendformatwidget(QDialog):
     def __init__(self, parent=None, title=''):#, arr=None):
