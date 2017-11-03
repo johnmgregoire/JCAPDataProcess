8cbe3c8f6ac7df390a6ef734f749079f3d1d52be
johnmgregoire <john.gregoire@solarfuleshub.org>
2017-11-03 13:03:25 -0700

    get_raw_data_from_exp options
    can specify a search string for filenames and give a sample_no list
    "sample_list" so only data with those samples are returned

--- BatchProcesses/get_raw_data_from_exp.py
+++ BatchProcesses/get_raw_data_from_exp.py
@@ -6,15 +6,11 @@ sys.path.append(os.path.join(projectroot,'QtForms'))
 sys.path.append(os.path.join(projectroot,'AuxPrograms'))
 sys.path.append(os.path.join(projectroot,'OtherApps'))
 
-from CreateExperimentApp import expDialog
-from CalcFOMApp import calcfomDialog
-from VisualizeDataApp import visdataDialog
-from VisualizeBatchFcns import choosexyykeys
 from fcns_io import *
 
 #from get_raw_data_from_exp import get_file_dicts_containing_data
 
-def get_file_dicts_containing_data(expname, filekeystoget, filetype):
+def get_file_dicts_containing_data(expname, filekeystoget, filetype, sample_list=None, fn_must_contain=''):
     exppath=buildexppath(expname)
     expd, expzipclass=readexpasdict(exppath, includerawdata=False, erroruifcn=None, returnzipclass=True)
     expfolder=os.path.split(exppath)[0]
@@ -28,6 +24,10 @@ def get_file_dicts_containing_data(expname, filekeystoget, filetype):
             if not fk in rund.keys():
                 continue
             for fn, filed in rund[fk][filetype].iteritems():
+                if not fn_must_contain in fn:
+                    continue
+                if (not sample_list is None) and (not filed['sample_no'] in sample_list):
+                    continue
                 allfilesdict[fn]=filed
                 filed['fn']=fn
                 ans=buildrunpath_selectfile(fn, expfolder, runp=rund['run_path'], expzipclass=expzipclass, returnzipclass=True)
@@ -41,10 +41,10 @@ def get_file_dicts_containing_data(expname, filekeystoget, filetype):
         filed['data_arr']=getarrs_filed(filed['fn'], filed, selcolinds=None, trydat=False, zipclass=filed['zipclass'])
     return allfilesdict
     
-###Demo
+####Demo
 #expname=r'/eche/20170504.082546.copied-20170504220706403PDT.zip'
 #filekeystoget=['files_technique__CV2']
 #filetype='pstat_files'
 #
-#allfilesdict=get_file_dicts_containing_data(expname, filekeystoget, filetype)
+#allfilesdict=get_file_dicts_containing_data(expname, filekeystoget, filetype, sample_list=[646, 941])
 #print 'the first data array has shape ', allfilesdict.values()[0]['data_arr'].shape
