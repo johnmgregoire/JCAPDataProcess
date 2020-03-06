7e6e532e7900bee0f73aa04cd1a2464c34ba75d6
U-HTEJCAP\guevarra <guevarra@hte-devw-09.htejcap.caltech.edu>
2020-03-06 13:08:56 -0800

    pass 'select_fom_keys' to merge_interp_xrfs_single_plate_id, batch xtrn-xrfs merge example

--- BatchProcesses/merge_interp_xrfs_single_plate_id.py
+++ BatchProcesses/merge_interp_xrfs_single_plate_id.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     sys.path.append(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0])
     sys.path.append(os.path.join(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0], 'AuxPrograms'))
     sys.path.append(os.path.join(os.path.split(os.path.split(os.path.realpath(__file__))[0])[0], 'AnalysisFunctions'))
-    
+
 from fcns_io import *
 from fcns_ui import *
 
 
-    
-def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, l_anak_to_merge=['ana__1'],xrfs_ana_int='highest containing search string', atfrac_search_string='AtFrac', interpmerge=True, save_extension='.run'):
+
+def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, fom_keys='ALL', l_anak_to_merge=['ana__1'],xrfs_ana_int='highest containing search string', atfrac_search_string='AtFrac', interpmerge=True, save_extension='.run'):
     if not ananame is None:#send None if already importana in calcui so extra import -> copy avoided
         anapath=buildanapath(ananame)
         calcui.importana(p=anapath)
@@ -20,17 +20,17 @@ def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, l_anak_
         print 'aborting xrfs merge, only written for single plate_id and in this ana includes :', pidstr
         return None
     infod=importinfo(pidstr)
-    #for ank in sort_dict_keys_by_counter(infod['analyses'], keystartswith='analyses__'):#use 
+    #for ank in sort_dict_keys_by_counter(infod['analyses'], keystartswith='analyses__'):#use
     analysesd=infod['analyses']
     xrfstups=sorted([(time.strptime(v['created_at'].rpartition(' ')[0],'%Y-%m-%d %H:%M:%S'), v) for k, v in analysesd.iteritems() if v['type']=='xrfs'])
     if len(xrfstups)==0:
         print 'aborting xrfs merge, xrfs data for ', pidstr
         return None
     relapth_xrfsana=xrfstups[-1][1]['path']#latest created_at analyses__ dict
-    
+
     calcui.importauxexpana(relapth_xrfsana, exp=False)
     xrfs_anad=calcui.aux_ana_dlist[0]
-    
+
     if not isinstance(xrfs_ana_int, int):
         anakeys=sort_dict_keys_by_counter(xrfs_anad)
         for anak in anakeys[::-1]:
@@ -45,7 +45,7 @@ def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, l_anak_
     if not isinstance(xrfs_ana_int, int):
         print 'aborting xrfs merge, no ana__ found containing more than 1 fom with ', atfrac_search_string
         return None
-    
+
     anakeys0=sort_dict_keys_by_counter(calcui.anadict)
     for anak_to_merge in l_anak_to_merge:
         for i in range(1, int(calcui.FOMProcessNamesComboBox.count())):
@@ -55,16 +55,17 @@ def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, l_anak_
                 calcui.processeditedparams()
                 break
         c=calcui.analysisclass
-        
+
         c.params['select_aux_keys']=atfrac_search_string
         c.params['select_aux_ints']=`xrfs_ana_int`
-        
+        c.params['select_fom_keys']=fom_keys
+
         if interpmerge:
             c.params['interp_is_comp']=1
         else:
             print 'aborting xrfs merge, asked for direct merge but not supported yet'
             return None
-    
+
         c.params['select_ana']=anak_to_merge
         c.processnewparams(calcFOMDialogclass=calcui, recalc_filedlist=True)
         calcui.analyzedata()
@@ -75,7 +76,7 @@ def merge_interp_xrfs_single_plate_id(calcui, ananame=None, pidstr=None, l_anak_
             return None
         else:
             print 'continuing with xrfs merge but not all requested ana__ were succesfully merged'
-    
+
     if not save_extension is None:
         newanasavefolder=calcui.saveana(dontclearyet=False, anatype=calcui.anadict['analysis_type'], rundone=save_extension)
         newanapath=buildanapath(newanasavefolder)
---
+++ BatchProcesses/xtrn_multi_process_interpmerge_xrfs_v1.py
@@ -0,0 +1,72 @@
+import sys
+import os
+from time import sleep
+############
+projectroot = os.path.split(os.getcwd())[0]
+sys.path.append(projectroot)
+sys.path.append(os.path.join(projectroot, 'QtForms'))
+sys.path.append(os.path.join(projectroot, 'AuxPrograms'))
+sys.path.append(os.path.join(projectroot, 'OtherApps'))
+
+
+from DBPaths import *
+from SaveImagesApp import *
+from fcns_ui import *
+from fcns_io import *
+from VisualizeBatchFcns import choosexyykeys
+from VisualizeDataApp import visdataDialog
+from CalcFOMApp import calcfomDialog
+from CreateExperimentApp import expDialog
+from merge_interp_xrfs_single_plate_id import merge_interp_xrfs_single_plate_id
+
+
+class MainMenu(QMainWindow):
+    def __init__(self, previousmm, execute=True):  # , TreeWidg):
+        super(MainMenu, self).__init__(None)
+        self.setWindowTitle('HTE Experiment and FOM Data Processing')
+        self.calcui = calcfomDialog(
+            self, title='Calculate FOM from EXP', guimode=False)
+
+
+interpd = {}
+with open('D:/TRI-XAS/runs/ana_interp_list_xtrn.tsv', 'r') as f:
+    for l in f.readlines():
+        p = l.strip().split()
+        interpd[p[0]] = p[1]
+
+for anats, keylist in interpd.items():
+    # try:
+    if 1:
+        mainapp = QApplication(sys.argv)
+        form = MainMenu(None)
+        calcui = form.calcui
+        print(anats, keylist)
+        merge_interp_xrfs_single_plate_id(
+            calcui=calcui,
+            ananame='xtrn/'+anats,
+            pidstr=None,
+            fom_keys='xw.mm,yw.mm,cryohor.mm,cryovert.mm,distance.mm',
+            l_anak_to_merge=[keylist],
+            xrfs_ana_int='highest containing search string',
+            atfrac_search_string='AtFrac',
+            interpmerge=True,
+            save_extension='.done')
+
+        # anasavefolder = calcui.saveana(
+        #     dontclearyet=False, anatype=anadestchoice, rundone=anasaveextension)
+        calcui.close()
+        sleep(0.5)
+        mainapp.quit()
+        sleep(0.5)
+        del calcui
+        del mainapp
+    # except:
+    #     print('Error processing %s' % (anats))
+    #     calcui.close()
+    #     sleep(0.5)
+    #     mainapp.quit()
+    #     sleep(0.5)
+    #     del calcui
+    #     del mainapp
+
+# openwindow()
