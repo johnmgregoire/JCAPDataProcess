1a721f1411ed4f9b4d8bbf46527c32c06b2aa9dd
onepunchdan <onepunchdan@users.noreply.github.com>
2022-03-30 11:45:39 -0700

    restore missing batch.py (wasn't tracked?)

---
+++ BatchProcesses/batch.py
@@ -0,0 +1,11 @@
+#update exp/anadestchoice to uvis when ready to run for real
+#set forceexp/ana to True to create .exp or .ana even if in batch file
+skiponerror=1
+batchfolder=r'K:\experiments\uvis\drop'
+batchinput_fn='batch.txt'
+expdestchoice=r'uvis'
+anadestchoice=r'uvis'
+forceexp=True
+forceana=True
+getpvdbool=True
+batchmode=True
